f0306cb31287809a43d411a0fcf1cebefe2dc938
Troy Astorino <troyastorino@gmail.com>
2011-11-12 19:12:20 -0500

    Adding first run at mobile version

--- src/collabow/views/canvas.clj
+++ src/collabow/views/canvas.clj
@@ -1,8 +1,17 @@
 (ns collabow.views.canvas
   (:require [collabow.views.common :as common])
   (:use [noir.core :only [defpage]]
-        [hiccup.core :only [html]]))
+        [hiccup.core :only [html]]
+        [hiccup.form-helpers :only [form-to text-field submit-button]]))
 
 (defpage "/ink" []
-  (common/layout [:jquery-ink :ink.js]
-   [:div#myInk]))
+  (common/layout
+   [:jquery-ink :ink.js]
+   [:div#my-ink]
+   [:div#data-form
+    (form-to [:post "/ink/data"]
+             (text-field "data")
+             (submit-button "Submit data"))]))
+
+(defpage [:post "/ink/data"] {:as data}
+  [:p data])
