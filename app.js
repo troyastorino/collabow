dc8b9ad63d331467ba619f4629b572a8d163016e
Troy Astorino <troyastorino@gmail.com>
2012-01-13 06:46:27 -0500

    had wrong port for heroku

--- app.js
+++ app.js
@@ -24,11 +24,13 @@ app.configure(function(){
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
   mongo.mongoose.connect("mongodb://localhost/emails");
+  app.listen(3000);
 });
 
 app.configure('production', function(){
   app.use(express.errorHandler());
   mongo.mongoose.connect(process.env.MONGOLAB_URI);
+  app.listen(process.env.PORT);
 });
 
 // Routes
@@ -37,5 +39,4 @@ app.get('/', routes.index);
 
 app.post('/email', routes.email)
 
-app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
